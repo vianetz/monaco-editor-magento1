4b03b171d22bd654494453c1c349c74c338f6610
Christoph Massmann <cm@vianetz.com>
2023-11-25 10:19:31 +0100

    moved textarea config into config.xml so it can be easily extended by other extensions, integrated editor assets on all pages where tinymce editor is used

--- app/code/local/MM/MonacoEditor/Block/Init.php
+++ app/code/local/MM/MonacoEditor/Block/Init.php
@@ -9,34 +9,12 @@ class MM_MonacoEditor_Block_Init extends Mage_Core_Block_Template
         $textareas = [];
         $controller = Mage::app()->getRequest()->getControllerName();
 
-        switch ($controller) {
-            case 'cms_page':
-                $textareas = [
-                    [
-                        'id' => 'page_content',
-                        'language' => 'html'
-                    ],
-                    [
-                        'id' => 'page_layout_update_xml',
-                        'language' => 'xml'
-                    ],
-                    [
-                        'id' => 'page_custom_layout_update_xml',
-                        'language' => 'xml'
-                    ],
-                ];
-                break;
-            case 'cms_block':
-                $textareas = [
-                    [
-                        'id' => 'block_content',
-                        'language' => 'html'
-                    ]
-                ];
-                break;
+        $config = Mage::getStoreConfig('cms/mm_monacoeditor/textarea_config');
+        if (isset($config[$controller])) {
+            $textareas = $config[$controller];
         }
 
-        return Zend_Json::encode($textareas, false, ['enableJsonExprFinder' => true]);
+        return Zend_Json::encode(array_values($textareas), false, ['enableJsonExprFinder' => true]);
     }
 
     public function isWysiwygEnabledByDefault(): bool
